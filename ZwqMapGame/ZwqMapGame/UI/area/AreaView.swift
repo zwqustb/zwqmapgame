0428e3414dfe7dd006f1bb27d308a6aa47d6c8f2
zhangwenqiang <10165571@boe.com.cn>
2019-11-21 14:45:33 +0800

    area view

---
+++ ZwqMapGame/ZwqMapGame/UI/area/AreaView.swift
@@ -0,0 +1,21 @@
+//
+//  AreaView.swift
+//  ZwqMapGame
+//
+//  Created by boeDev on 2019/11/21.
+//  Copyright © 2019 zwq. All rights reserved.
+//
+
+import UIKit
+
+class AreaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
